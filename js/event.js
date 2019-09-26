4519a4e5f6bf9ce97465c2a77731a4673bde4016
TCutter <31432487+TCutter@users.noreply.github.com>
2019-09-26 13:54:21 +0800

    绘制立方体

--- js/event.js
+++ js/event.js
@@ -1,12 +1,22 @@
 var g_eyeX = 0.2, g_eyeY = 0.25, g_eyeZ = 0.25 // 视点
+var g_near = -1.0, g_far = 1.0
 
 function keydown (ev) {
-  if (ev.keyCode === 39) {
-    g_eyeZ += 0.1
-  } else if (ev.keyCode === 37) {
-    g_eyeZ -= 0.1
-  } else {
-    return
+  switch (ev.keyCode) {
+    case 37:
+      g_eyeX -= 0.1
+      break
+    case 38:
+      g_far -= 0.1
+      break
+    case 39:
+      g_eyeX += 0.1
+      break
+    case 40:
+      g_far += 0.1
+      break
+    default:
+      break
   }
   draw()
 }
--- js/renderer.js
+++ js/renderer.js
@@ -14,9 +14,6 @@ class Renderer {
   draw () {
     var webgl = Renderer.sWebgl
     webgl.useProgram(Renderer.sProgram) // 要放在片段着色器前面调用
-
-    // this._setModelMatrix()
-    // this._setViewMatrix()
     this._setViewModelMatrix()
     this._doDraw()
   }
@@ -59,76 +56,79 @@ class Renderer {
   _initVertexBuffers () {
     var webgl = Renderer.sWebgl
 
-    var vertices = new Float32Array([
+    var vertices = [
       // 顶点坐标和颜色      
-      0.0, 0.5, -0.4, 0.4, 1.0, 0.4, // 绿色三角形在最后面
-      -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
-      0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
+      1.0, 1.0, 1.0, 1.0, 1.0, 1.0, // v0, 白色
+      -1.0, 1.0, 1.0, 1.0, 1.0, 0.0,
+      -1.0, -1.0, 1.0, 1.0, 0.0, 0.0,
+      1.0, -1.0, 1.0, 1.0, 0.0, 1.0,
+      1.0, -1.0, -1.0, 0.0, 1.0, 1.0,
+      1.0, 1.0, -1.0, 0.0, 1.0, 0.0,
+      -1.0, 1.0, -1.0, 0.0, 0.0, 1.0,
+      -1.0, -1.0, -1.0, 0.0, 0.0, 0.0 // v7
+    ]
+    vertices = vertices.map(item => item / 2)
+    vertices = new Float32Array(vertices)
 
-      0.5, 0.4, -0.2, 1.0, 0.4, 0.4, // 黄色三角形在中间
-      -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
-      0.0, -0.6, -0.2, 1.0, 1.0, 0.4,
+    // var indices = new Uint8Array([
+    //   0, 1, 2, 0, 2, 3, // 前
+    //   0, 3, 4, 0, 4, 5, // 右
+    //   0, 5, 6, 0, 6, 1, // 上
+    //   1, 6, 7, 1, 7, 2, // 左
+    //   7, 4, 3, 7, 3, 2, // 下
+    //   4, 7, 6, 4, 6, 5 // 后
+    // ])
 
-      0.0, 0.5, 0.0, 0.4, 0.4, 1.0, // 蓝色三角形在最前面
-      -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
-      0.5, -0.5, 0.0, 1.0, 0.4, 0.4
+    // 调换面的顺序也没有影响
+    var indices = new Uint8Array([
+      0, 1, 2, 0, 2, 3, // 前
+      7, 4, 3, 7, 3, 2, // 下
+      0, 5, 6, 0, 6, 1, // 上
+      1, 6, 7, 1, 7, 2, // 左
+      0, 3, 4, 0, 4, 5, // 右
+      4, 7, 6, 4, 6, 5 // 后
     ])
 
+    
     // 创建缓冲区对象
-    var buffer = webgl.createBuffer()
+    var buffer = webgl.createBuffer()    
+    // 将顶点坐标和颜色写入缓冲区对象
     webgl.bindBuffer(webgl.ARRAY_BUFFER, buffer)
     webgl.bufferData(webgl.ARRAY_BUFFER, vertices, webgl.STATIC_DRAW)
     const FSIZE = vertices.BYTES_PER_ELEMENT
-
     var a_Position = webgl.getAttribLocation(Renderer.sProgram, 'a_Position')
     webgl.vertexAttribPointer(a_Position, 3, webgl.FLOAT, false, FSIZE * 6, 0)
     webgl.enableVertexAttribArray(a_Position) 
-
     var a_Color = webgl.getAttribLocation(Renderer.sProgram, 'a_Color')
     webgl.vertexAttribPointer(a_Color, 3, webgl.FLOAT, false, FSIZE * 6, FSIZE * 3)
     webgl.enableVertexAttribArray(a_Color) 
+
+    // 将顶点索引数据写入缓冲区对象
+    var indexBuffer = webgl.createBuffer()
+    webgl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+    webgl.bufferData(webgl.ELEMENT_ARRAY_BUFFER, indices, webgl.STATIC_DRAW)
   }
 
-  // 设置模型视图矩阵，将 u_ViewMatrix 和 u_ModelMatrix 计算后的结果传给着色器
+  // 设置模型视图矩阵和正交投影矩阵，将 u_ViewMatrix 和 u_ModelMatrix 以及 u_ProjMatrix 计算后的结果传给着色器
   _setViewModelMatrix () {
     var gl = Renderer.sWebgl
 
-    var viewMatrix = mat4.lookAt(
-      [g_eyeX, g_eyeY, g_eyeZ],
-      [0, 0, 0],
-      [0, 1, 0]
-    )
-
     var modelMatrix = mat4.create()
     mat4.identity(modelMatrix)
-    mat4.rotateZ(modelMatrix, -Math.PI * 10 / 180)
-    
-    var matrix = mat4.multiply(viewMatrix, modelMatrix)
-    var u_Matrix = gl.getUniformLocation(Renderer.sProgram, 'u_Matrix')
-    gl.uniformMatrix4fv(u_Matrix, false, matrix)
-  }
 
-  // 设置视图矩阵
-  _setViewMatrix () {
-    var gl = Renderer.sWebgl
-    var u_ViewMatrix = gl.getUniformLocation(Renderer.sProgram, 'u_ViewMatrix')
-
-    // 设置视点、视线和上方向
-    var matrix = mat4.lookAt(
-      [0.2, 0.25, 0.25],
+    // 视图矩阵
+    var viewMatrix = mat4.lookAt(
+      [g_eyeX, 0.25, 0.25],
       [0, 0, 0],
       [0, 1, 0]
     )
-    gl.uniformMatrix4fv(u_ViewMatrix, false, matrix)
-  }
-  // 设置模型矩阵
-  _setModelMatrix () {
-    var gl = Renderer.sWebgl
-    var u_ModelMatrix = gl.getUniformLocation(Renderer.sProgram, 'u_ModelMatrix')
-    var matrix = mat4.create()
-    mat4.identity(matrix)
-    mat4.rotateZ(matrix, -Math.PI * 10 / 180)
-    gl.uniformMatrix4fv(u_ModelMatrix, false, matrix)
+    // var matrix = mat4.multiply(viewMatrix, modelMatrix)
+    // 正交投影矩阵
+    var projMatrix = mat4.ortho(-1, 1, -1, 1, g_near, g_far)
+    var matrix = mat4.multiply(projMatrix, mat4.multiply(viewMatrix, modelMatrix))
+    
+    var u_Matrix = gl.getUniformLocation(Renderer.sProgram, 'u_Matrix')
+    gl.uniformMatrix4fv(u_Matrix, false, matrix)
   }
 
   _doDraw () {
@@ -138,9 +138,11 @@ class Renderer {
     webgl.canvas.height = this._canvas.height
     webgl.viewport(0, 0, webgl.canvas.width, webgl.canvas.height)
 
-    webgl.clearColor(0.0, 1.0, 1.0, 1.0)
-    webgl.clear(webgl.COLOR_BUFFER_BIT)
-    webgl.drawArrays(webgl.TRIANGLES, 0, 9) // 绘制三角形，从第1个顶点开始绘制3个顶点
+    webgl.enable(webgl.DEPTH_TEST) // 开启隐藏面消除
+
+    webgl.clearColor(1.0, 1.0, 1.0, 1.0)
+    webgl.clear(webgl.COLOR_BUFFER_BIT | webgl.DEPTH_BUFFER_BIT)
+    webgl.drawElements(webgl.TRIANGLES, 36, webgl.UNSIGNED_BYTE, 0) // 绘制三角形，从第1个顶点开始绘制3个顶点
 
     var ctx = this._canvas.getContext('2d')
     ctx && ctx.drawImage(Renderer.sCanvas, 0, 0)
@@ -152,8 +154,6 @@ Renderer.sVshPgm = [
   'precision mediump float;',
   'attribute vec4 a_Position;',
   'attribute vec4 a_Color;',
-  // 'uniform mat4 u_ModelMatrix;',
-  // 'uniform mat4 u_ViewMatrix;',
   'uniform mat4 u_Matrix;',
   'varying vec4 v_Color;',
   'void main()',
@@ -166,7 +166,6 @@ Renderer.sVshPgm = [
 // fragment shader program
 Renderer.sFshPgm = [
   'precision mediump float;',
-  'varying vec2 v_TexCoord;',
   'varying vec4 v_Color;',
   'void main()',
   '{',
